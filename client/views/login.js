4ea1d8a6a6997b17eea5d732d1f943b2679fbfe0
David Gomes <davidrafagomes@gmail.com>
2014-08-05 12:43:08 +0100

    Fixes login.js code style.

--- client/views/login.js
+++ client/views/login.js
@@ -1,56 +1,47 @@
-//Validate Email Function
-
-function validateEmail (email) {
-  var filter = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return filter.test(email);
-}
-
-//Template Code
-
 Template.signupForm.events({
-  "submit #signup-form": function (event, template) {
+  'submit #signup-form': function (event, template) {
     event.preventDefault();
-    if (template.find("#signup-username").value.length < 4 || template.find("#signup-password").value.length < 6 || !validateEmail(template.find("#signup-email").value)) {
-      console.log("One or more parameters are invalid");
-    }
-    else{
-      Accounts.createUser({
-        username: template.find("#signup-username").value,
-        password: template.find("#signup-password").value,
-        profile: {
-          email: template.find("#signup-email").value
-          // Other required field values can go here
-        }
-      }, function (error) {
-        if (error) {
-          console.log("Error when creating account: " + error);
-        }
-      });
-    }
+
+    Accounts.createUser({
+      username: $('#signup-username').val(),
+      password: $('#signup-password').val(),
+
+      profile: {
+        email: template.find('#signup-email').val()
+        // Other required field values can go here
+      }
+    }, function (error) {
+      if (error) {
+        console.log('An error occurred creating an account: ' + error);
+      }
+    });
   }
 });
 
 Template.loginForm.events({
-  "submit #login-form": function (event, template) {
+  'submit #login-form': function (event, template) {
     event.preventDefault();
+
     Meteor.loginWithPassword(
-      template.find("#login-username").value,
-      template.find("#login-password").value,
+      $('#login-username').val(),
+      $('#login-password').val(),
+
       function (error) {
         if (error) {
-          consoe.log("Error during logging in: " + error);
+          consoe.log('An error occurred logging in: ' + error);
         }
       }
-      );
+    );
   }
 });
 
 Template.logoutForm.events({
-  "click #logout-form": function (event, template) {
+  'click #logout-form': function (event, template) {
     event.preventDefault();
+
     Meteor.logout(function (error) {
       if (error) {
-        console.log("Can not logout: " + error);
+        console.log('An error occurred logging out: ' + error);
       }
     });
   }
